ba885aa262762736a5566a9fbe8e1dd473dd2c52
yanyu <yanyu@jk.cn>
2016-08-22 11:21:47 +0800

    update api key

--- inc/info.php
+++ inc/info.php
@@ -3,9 +3,9 @@
 $ym='http://'.$_SERVER['SERVER_NAME'].'/';//你的站点域名。不可为空，否则解析器无法工作
 $parser='parser';//解析器目录,默认parser，如果本程序不是安装在根目录请加上上级路径，如abc/parser
 $home_v='UCAuUUnT6oDeKwE6v1NGQxug';//首页推荐视频用户身份标示
-$youtube_api='AIzaSyAntBZOF6TTjM-1U_dQ6bQqZEhsTYfHunA';//youtube视频API
-$gurl_api='AIzaSyAntBZOF6TTjM-1U_dQ6bQqZEhsTYfHunA';//谷歌缩址api，用于防止dns污染
-$title='Just For Fun';//您的网站名字
+$youtube_api='AIzaSyDrNE03l_as61mM0kDFgd3nRWUUWgtUUnQ';//youtube视频API
+$gurl_api='AIzaSyDrNE03l_as61mM0kDFgd3nRWUUWgtUUnQ';//谷歌缩址api，用于防止dns污染
+$title='Joy and Fun';//您的网站名字
 
 //seo设置
 $keyw_ords='';//keywords头部关键词设置
